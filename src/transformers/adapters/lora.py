efe6ab498d9acd633e20fd8e1b6000073c24e26b
Dainis Boumber <dainis.boumber@gmail.com>
2024-06-12 05:38:06 +0000

    m

--- src/transformers/adapters/lora.py
+++ src/transformers/adapters/lora.py
@@ -302,7 +302,7 @@ class Linear(LoRALayer, nn.Linear):
                                 norm_w = delta_w.norm(p=2, dim=-1, keepdim=True) + 1e-9
                                 unit_w = delta_w / norm_w
                                 direction = delta_w * unit_w
-                                delta_w = direction *(lora.scaling * lora.lora_alpha + 1e-9)
+                                delta_w = direction * torch.t(lora.m) * (lora.scaling * lora.lora_alpha + 1e-9)
                                 
                                 
                             delta_w = delta_w.view(1, 1, -1)
